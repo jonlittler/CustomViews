d8fb7e88b8b4d3a0929f10c360ce2c8aadc1a05c
Jon Littler <jonlittler@mac.com>
2016-12-29 18:12:19 -0500

    Initial Commit

---
+++ CustomViews/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CustomViews
+//
+//  Created by Jon Littler on 12/29/16.
+//  Copyright © 2016 Jon Littler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
